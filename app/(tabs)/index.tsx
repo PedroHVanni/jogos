127fa3b3f488d4510815f002afd387a9a60b712a
Pedro Henrique Vanni <ra2457015@mac403.local>
2026-05-27 21:24:31 -0300

    27/05/2026

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,16 +4,20 @@ import { StyleSheet, View } from "react-native";
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
 
 const PlaceholderImage = require('../../assets/images/a0c55db70393bea13a4746c8aab145c8.jpg');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false); 
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       quality: 1,
     });
@@ -26,12 +30,36 @@ export default function Index() {
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // Lógica para salvar a imagem editada
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reiniciar" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Salvar" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : selectedImage ? (
         <View />
       ) : (
       <View style={styles.footerContainer}>
@@ -39,6 +67,9 @@ export default function Index() {
         <Button label="Usar esta foto" onPress={() => setShowAppOptions(true)} />
       </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* Aqui você pode adicionar os emojis para serem selecionados */}
+      </EmojiPicker>
     </View>
   );
 }
@@ -55,5 +86,13 @@ const styles = StyleSheet.create({
       footerContainer: {
         flex: 1 / 3,
         alignItems: 'center',
+      },
+      optionsContainer: {
+        position: 'absolute',
+        bottom: 80,
+      },
+      optionsRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
       },
 });
